45f04b95dd97d401b48672d16bbf5971b7f36415
Giovana Ferreira <61763524+Giovana22@users.noreply.github.com>
2020-05-26 20:19:22 -0300

    Add files via upload

---
+++ OlaMundo.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : OlaMundo.cpp
+// Author      : Giovana
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+
+	// Laço ou loop
+	bool cheia;
+
+	for (int contador = 0; contador < 1000; contador++) {
+			std::cout << "Encha mais a garrafa!!" << std::endl; // prints !!!Hello World!!!
+	}
+
+
+
+
+	return 0;
+}
